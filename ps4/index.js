715edb46b68f28849fa489f91f25dbc88005704c
Fujii Shuto <f-shuto@Virgo.local>
2019-06-08 12:27:05 +0900

    楽天TVとyoutubeとキングダムハーツを追加

--- ps4/index.js
+++ ps4/index.js
@@ -9,6 +9,7 @@ const { Device } = require('ps4-waker');
 const TORNE      = "CUSA00442";
 const KH3        = "CUSA11060";
 const RAKUTEN_TV = "CUSA11244";
+const YOUTUBE    = "CUSA01065";
 
 // 実行時引数で指定する
 let port     = process.argv[2];
@@ -50,15 +51,25 @@ function ExecPS4(topic, message) {
 			if (data === undefined) return;
 			switch (data.app) {
 				case 'トルネ':
+				case 'torne':
 					TurnOnTitle(TORNE);
 					break;
 
 				case 'キングダムハーツ':
+				case 'キングダム ハーツ':
 					TurnOnTitle(KH3);
 					break;
 
 				case '楽天TV':
+				case '楽天 TV':
+				case '楽天':
+				case 'らくてん':
 					TurnOnTitle(RAKUTEN_TV);
+					break;
+	
+				case 'youtube':
+				case 'YouTube':
+					TurnOnTitle(YOUTUBE);
 					break;
 
 				default:
